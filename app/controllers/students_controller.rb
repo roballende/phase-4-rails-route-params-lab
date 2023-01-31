85f41f138f64b5f6a80a32bb7eb3e8d9009243a9
Robert Allende <allende.rob@gmail.com>
2023-01-31 12:25:09 -0600

    commit tree baa267e9f9cbad0cd34d54ad0c176f2e00749386

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,34 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = if params[:name]
+    #   Student.by_name(params[:name])
+    # else
+    #   Student.all
+    # end  
+
+    students = if params[:first_name]
+        Student.find_by(first_name: params[:first_name])
+      elsif params[:last_name]
+        Student.find_by(last_name: params[:last_name])
+      else
+        Student.all
+      end
+
+    # students = if params[:first_name :last_name]
+    #     Student.find_by(first_name: params[:first_name] last_name: params[:last_name])
+    #   else
+    #     Student.all
+    #   end
+
+
+
     render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
